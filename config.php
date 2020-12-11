84cd86f2d6efcd9a94fd189125ece2854e5c7ddb
Rafael <57455645+RafaelLH8@users.noreply.github.com>
2020-12-11 12:04:55 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	session_start();
+	define("HOME_URI", "http://localhost/mongo-func/");
+	define("HOME_DIR", "C:\\xampp\htdocs\mongo-func\\");
+	require_once __DIR__ . '/vendor/autoload.php';
+?>
